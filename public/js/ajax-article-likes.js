5addbfc6db6d7070467fca581eb8612a6ed9581c
jerome <ambroise.jerome.formation@gmail.com>
2019-04-11 16:55:16 +0200

    AJAX likes

--- public/js/ajax-article-likes.js
+++ public/js/ajax-article-likes.js
@@ -1,13 +1,18 @@
 console.log('JS bien chargé');
 
-const params = {
-    url: 'http://blog2.local/api/article/likes/2'
-};
+$('#likes button').on('click', function() {
+    // Récupération de l'id
+    const id = $('#likes button').first().data('id');
 
-// Lancement de l'appel AJAX
-$.ajax(params).done(displayNewLikes);
+    const params = {
+        url: 'http://blog2.local/api/article/likes/' + id
+    };
+
+    // Lancement de l'appel AJAX
+    $.ajax(params).done(displayNewLikes);
+});
 
 function displayNewLikes(json) {
     // Afficher la nouvelle valeur du nombre de likes
-    console.log('Retour de l\'appel AJAX');
+    $('#likes span').text(json.cpt);
 }
