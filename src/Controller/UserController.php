ad9f2afa4a0d749210503d46f06e0fad15d0fcb8
jerome <ambroise.jerome.formation@gmail.com>
2019-04-11 11:51:38 +0200

    Acces au detail d'un utilisateur en BDD (TWIG vide)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,9 +18,29 @@ class UserController extends AbstractController
         $repo = $this->getDoctrine()->getRepository(User::class);
         // Récupération des utilisateurs
         $users = $repo->findAll();
-        // Envoi des utilisateurs à la vue
+        // Renvoi des utilisateurs à la vue
         return $this->render('user/list.html.twig', [
             'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/back-office/utilisateurs/changement-roles/{id}",name="app_user_editrole", requirements={"id"="[0-9]+"})
+     * @param int $id
+     * @return Response
+     */
+    public function edit(int $id): Response
+    {
+        // Récupération du Repository
+        $repo = $this->getDoctrine()->getRepository(User::class);
+        // Récupération des utilisateurs
+        $user = $repo->findOneBy([
+            'id' => $id
+        ]);
+
+        // Renvoi des utilisateurs à la vue
+        return $this->render('user/show.html.twig', [
+            'user' => $user
         ]);
     }
 }
