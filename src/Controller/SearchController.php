efd99a0d166c3645b3d1e1dc55980b7c3dc9ed0c
jerome <ambroise.jerome.formation@gmail.com>
2019-04-18 11:34:48 +0200

    Creation formulaire de recherche d une personne

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Personne;
+use App\Form\PersonneType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function index(Request $request)
+    {
+        $personne = new Personne();
+        $form = $this->createForm(PersonneType::class, $personne);
+
+        dump('ici');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            dump('la');
+            $personne =  $this->getDoctrine()
+                ->getRepository(Personne::class)
+                ->findPeoples($personne);
+
+            dump($personne);
+            die();
+        }
+
+        return $this->render('search/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/PersonneType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Personne;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', null, [
+                'required' => false
+            ])
+            ->add('lastname', null, [
+                'required' => false
+            ])
+            ->add('profession', null, [
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Personne::class,
+        ]);
+    }
+}
